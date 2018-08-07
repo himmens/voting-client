4fe871abe208d83bba3b6956c1a710b8c4360fbf
Sergey Himanych <s.himanych@zmeke.com>
2018-08-07 17:38:26 +0300

    added socket connection

--- src/index.jsx
+++ src/index.jsx
@@ -2,23 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
+import io from 'socket.io-client';
 import reducer from './reducer';
 import App from './App';
 
 const store = createStore(reducer);
-store.dispatch({
-  type: 'SET_STATE',
-  state: {
-    vote: {
-      pair: ['Sunshine', '28 Days Later'],
-      tally: {Sunshine: 2}
-    }
-  }
-});
 
 ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
   document.getElementById('root')
+);
+
+const socket = io(`${location.protocol}//${location.hostname}:8090`);
+socket.on('state', state =>
+  store.dispatch({type: 'SET_STATE', state})
 );
